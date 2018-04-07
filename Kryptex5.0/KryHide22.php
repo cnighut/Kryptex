4cea0421f1a0211fb7cc7d3acf2dd03b5157b091
Chirag Nighut <qawbecrdteyf@gmail.com>
2018-04-07 18:20:55 +0530

    Update KryHide22.php

--- Kryptex5.0/KryHide22.php
+++ Kryptex5.0/KryHide22.php
@@ -17,7 +17,7 @@ $s=$_COOKIE['Username'];
 $k="ockhi";
 if($name==$k)
 {
-        $servername = "localhost;  
+        $servername = "localhost";  
        $username = "root";  
        $password="";   
        $conn = mysql_connect ($servername , $username , $password) or die("unable to connect to host");  
